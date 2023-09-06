2e42d72d49b3109f63bc8cbc6690b200df46ae84
Phani Pavan K <kphanipavan@gmail.com>
2023-09-06 19:27:44 +0530

    Re-enable release file generation

--- scripts/scrapeToJson.py
+++ scripts/scrapeToJson.py
@@ -74,5 +74,7 @@ if __name__ == "__main__":
     with open("data.dart", "w+") as dataWriter:
         allData = "String data = r'" + json.dumps(finalData) + "';"
         dataWriter.write(allData)
-    # with open("out.txt", "w+") as hashWriter:
-    #     hashWriter.write(hashlib.md5(json.dumps(finalData).encode("utf-8")).hexdigest())
+    with open("out.json", "w+") as jsonWriter:
+        json.dump(finalData, jsonWriter)
+    with open("out.txt", "w+") as hashWriter:
+        hashWriter.write(hashlib.md5(json.dumps(finalData).encode("utf-8")).hexdigest())
